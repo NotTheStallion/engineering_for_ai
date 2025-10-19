53d09c79881637e86a71a7f3a7c1cb2daa6c4e83
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-10-19 16:07:47 +0200

    [M] "slm_project/src/slm.py" : Removing temp function

--- slm_project/src/slm.py
+++ slm_project/src/slm.py
@@ -32,7 +32,3 @@ def slm_inference(message: str, max_new_tokens: int = 500, temperature: float = 
     response = decode(outputs[0])
     response = strip(response)
     return response
-
-
-def fix_function():
-    pass
